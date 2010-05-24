d050fbcc59e6b0c11e01ab337060580e6784d6f1
Christian <Christian@192.168.1.103>
2010-05-24 07:51:19 -0400

    going popup

--- dvdb-war/src/main/java/de/dvdb/web/item/action/v3/ItemController.java
+++ dvdb-war/src/main/java/de/dvdb/web/item/action/v3/ItemController.java
@@ -1,6 +1,12 @@
 package de.dvdb.web.item.action.v3;
 
+import java.util.Date;
+
+import javax.faces.event.ActionEvent;
+
+import org.jboss.seam.Component;
 import org.jboss.seam.ScopeType;
+import org.jboss.seam.annotations.Create;
 import org.jboss.seam.annotations.Logger;
 import org.jboss.seam.annotations.Name;
 import org.jboss.seam.annotations.Out;
@@ -8,6 +14,7 @@ import org.jboss.seam.annotations.Scope;
 import org.jboss.seam.log.Log;
 
 import de.dvdb.domain.model.item.type.Item;
+import de.dvdb.domain.model.mediabase.MediabaseItemCollectible;
 
 @Name("itemController")
 @Scope(ScopeType.PAGE)
@@ -18,16 +25,19 @@ public class ItemController {
 
 	@Out(required = false)
 	Item selectedItem;
-
-	public void doIt() {
-		log.info("Do it on {0}", selectedItem);
+	
+	@Out(scope = ScopeType.PAGE)
+	MediabaseItemCollectible mic;
+	
+	@Create
+	public void init() {
+		mic = (MediabaseItemCollectible)Component.getInstance(MediabaseItemCollectible.class, true);
 	}
 
 	public void test(Object o) {
 
 		selectedItem = (Item) o;
-		log.info("work on " + selectedItem);
-
+		log.info("work on " + o + " " + new Date());
+		
 	}
-
 }
